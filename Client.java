7faa4969798327bfd9d9837d347b186da4641709
Odelia J <odeliajoubert@gmail.com>
2021-02-25 09:45:23 +0200

    added client class

---
+++ Client.java
@@ -0,0 +1,47 @@
+public class Client {
+   private String firstName;
+   private String lastName;
+   private String cellNumber;
+   private String email;
+
+   public String getFirstName() {
+      return this.firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return this.lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getCellNumber() {
+      return this.cellNumber;
+   }
+
+   public void setCellNumber(String cellNumber) {
+      this.cellNumber = cellNumber;
+   }
+
+   public String getEmail() {
+      return this.email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+
+   public Client(String firstName, String lastName, String cellNumber, String email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.cellNumber = cellNumber;
+      this.email = email;
+   }
+
+}
