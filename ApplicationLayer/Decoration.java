40a6504f43c997950ff13f0ba5487038f75f5194
Odelia J <odeliajoubert@gmail.com>
2021-03-07 19:46:23 +0200

    Added to decoration class

--- ApplicationLayer/Decoration.java
+++ ApplicationLayer/Decoration.java
@@ -2,8 +2,51 @@ package ApplicationLayer;
 public class Decoration 
 {
    private Theme theme;
-   //private boolean balloons;
-   //private boolean candels;
-   //private int nrOfTables;
+   private boolean balloons;
+   private boolean candels;
+   private int nrOfTables;
+
+   //theme
+   public Theme GetTheme()
+   {
+      return this.theme;
+   }
+
+   public void SetEvent(Theme theme)
+   {
+      this.theme = theme;
+   }
+
+   //balloons
+   public Boolean GetBalloons()
+   {
+      return this.balloons;
+   }
    
+   public void SetBalloons(boolean balloon)
+   {
+      this.balloons = balloon;
+   }
+
+   //candles
+   public boolean GetCandles()
+   {
+      return this.candels;
+   }
+
+   public void SetCandles(boolean candle)
+   {
+      this.candels = candle;
+   }
+
+   //Tables
+   public int GetTables()
+   {
+      return this.nrOfTables;
+   }
+
+   public void SetTables(int table)
+   {
+      this.nrOfTables = table;
+   }
 }
