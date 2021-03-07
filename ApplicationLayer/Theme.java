9863c01dd0e3f5b91d3c9553aef8276ea6fad9e6
Odelia J <odeliajoubert@gmail.com>
2021-03-07 20:03:26 +0200

    Added to theme class

--- ApplicationLayer/Theme.java
+++ ApplicationLayer/Theme.java
@@ -4,5 +4,38 @@ public class Theme
    private String color1;
    private String color2;
    private String eventTheme;
-   //the themes can also include actual theses, like a pirate theme or hawaii theme
+
+   //theme
+   public String GetTheme()
+   {
+      return this.eventTheme;
+   }
+
+   public void SetTheme(String Etheme)
+   {
+      this.eventTheme = Etheme;
+   }
+
+   //color 1
+   public String GetColor1()
+   {
+      return this.color1;
+   }
+
+   public void SetColor1(String col1)
+   {
+      this.color1 = col1;
+   }
+
+   //color 2
+   public String GetColor2()
+   {
+      return this.color2;
+   }
+
+   public void SetColor2(String col2)
+   {
+      this.color2 = col2;
+   }
+
 }
